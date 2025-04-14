a000b967dc59d1a211033c56d06f7f9291e31788
Mohammed Ali <mohammed@aggregate-genius.com>
2025-04-14 14:00:02 +0000

    version 0.2.4

--- inst/doc/logViewer-module.R
+++ inst/doc/logViewer-module.R
@@ -1,12 +1,12 @@
 ## ----eval=F-------------------------------------------------------------------
-#  # Inside ui_body.R
-#  
-#  logViewerOutput("logViewerId")
+# # Inside ui_body.R
+# 
+# logViewerOutput("logViewerId")
 
 ## ----eval=F-------------------------------------------------------------------
-#  library(periscope2)
-#  
-#  app_dir = tempdir()
-#  create_application(name = 'mysampleapp', location = app_dir, sample_app = TRUE)
-#  runApp(paste(app_dir, 'mysampleapp', sep = .Platform$file.sep))
+# library(periscope2)
+# 
+# app_dir = tempdir()
+# create_application(name = 'mysampleapp', location = app_dir, sample_app = TRUE)
+# runApp(paste(app_dir, 'mysampleapp', sep = .Platform$file.sep))
 
